a9622c1d96f306b34efe1e365840bb60a6be3850
culpgrant <59777128+culpgrant@users.noreply.github.com>
2021-05-21 16:23:47 -0500

    added kf_consumer

--- Kafka_Files/kafka_consumer.py
+++ Kafka_Files/kafka_consumer.py
@@ -3,7 +3,7 @@ KAFKA_TOPIC = 'coinrank'
 
 
 consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest',
-                         enable_auto_commit=True, group_id='my-group',auto_commit_interval_ms=1000)
+                         enable_auto_commit=True, group_id='my-group', auto_commit_interval_ms=1000)
 for message in consumer:
     message = message.value
     print(message.decode('utf-8'))
